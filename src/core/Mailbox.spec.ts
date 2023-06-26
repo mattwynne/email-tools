81456e35ab4fc48f24cc58d0cb83b78ec0b9f725
Matt Wynne <matt@mattwynne.net>
2023-06-26 09:55:21 -0700

    Implement Mailbox#contactsGroup

--- src/core/Mailbox.spec.ts
+++ src/core/Mailbox.spec.ts
@@ -1,5 +1,15 @@
+import { assertThat, equalTo } from "hamjest"
 import { Mailbox } from "./Mailbox"
+import { ContactsGroup } from "./ContactsGroup"
 
 describe(Mailbox.name, () => {
-  it("works")
+  describe("mapping to contacts group", () => {
+    it("Removes the 'Inbox/' prefix", () => {
+      const mailbox = Mailbox.named("Inbox/Paperwork")
+      assertThat(
+        mailbox.contactsGroup,
+        equalTo(ContactsGroup.named("Paperwork"))
+      )
+    })
+  })
 })
