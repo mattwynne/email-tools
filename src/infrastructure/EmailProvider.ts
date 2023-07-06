276ba4ae8fb949583b27b5b96da81a3a03e7b279
Matt Wynne <matt@mattwynne.net>
2023-07-06 00:16:09 -0700

    Start actually calling fastmail

--- src/infrastructure/EmailProvider.ts
+++ src/infrastructure/EmailProvider.ts
@@ -1,4 +1,7 @@
+import { Mailbox } from "../core/Mailbox"
+import { MailboxName } from "../core/MailboxName"
 import { MailboxState } from "../core/MailboxState"
+import util from "util"
 
 const defaultNullConfiguration = {
   mailboxState: new MailboxState([]),
@@ -8,14 +11,128 @@ type NullEmailProviderConfiguration = {
   mailboxState: MailboxState
 }
 
+interface EmailAccount {
+  getMailboxState: () => Promise<MailboxState>
+}
+
+class NullEmailAccount implements EmailAccount {
+  constructor(private readonly mailboxState: MailboxState) {}
+
+  async getMailboxState() {
+    return this.mailboxState
+  }
+}
+
+export type FastmailConfig = {
+  token: string
+}
+
+type ApiMethod =
+  | "Email/get"
+  | "Email/changes"
+  | "Email/query"
+  | "Email/queryChanges"
+  | "Thread/changes"
+  | "Thread/get"
+  | "Mailbox/changes"
+  | "Mailbox/get"
+
+class FastmailEmailAccount implements EmailAccount {
+  constructor(private readonly session: FastmailSession) {}
+
+  private async methodCall(apiUrl: string, method: ApiMethod, params: Object) {
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: this.session.headers,
+      body: JSON.stringify({
+        using: ["urn:ietf:params:jmap:core", "urn:ietf:params:jmap:mail"],
+        methodCalls: [[method, params, "a"]],
+      }),
+    })
+    const data = await response.json()
+    const result = data["methodResponses"][0][1]
+    // console.log(
+    //   method,
+    //   "params:",
+    //   params,
+    //   "response:",
+    //   util.inspect(result, { depth: Infinity })
+    // )
+    return result
+  }
+
+  private async getMailboxes(): Promise<Mailbox[]> {
+    return (
+      await this.methodCall(this.session.apiUrl, "Mailbox/get", {
+        accountId: this.session.accountId,
+        ids: null,
+      })
+    ).list.map(
+      (mailbox: { name: string }) =>
+        new Mailbox(MailboxName.of(mailbox.name), [])
+    )
+  }
+
+  async getMailboxState() {
+    const mailboxes = await this.getMailboxes()
+    const inbox = mailboxes.find(
+      (mailbox) => mailbox.name === MailboxName.of("Inbox")
+    )
+    // const inboxChildren = mailboxes.filter(
+    //   (mailbox: { parentId: string }) => mailbox.parentId === inbox.id
+    // )
+    return new MailboxState(mailboxes)
+  }
+}
+
+class FastmailSession {
+  static async create(token: string) {
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    }
+    const hostname = "api.fastmail.com"
+    const authUrl = `https://${hostname}/.well-known/jmap`
+    const response = await fetch(authUrl, {
+      method: "GET",
+      headers,
+    })
+    return new this(headers, (await response.json()) as FastmailSession)
+  }
+
+  constructor(
+    public readonly headers: { [key: string]: string },
+    private readonly data: FastmailSession
+  ) {}
+
+  get apiUrl(): string {
+    return this.data.apiUrl
+  }
+
+  get primaryAccounts(): { [urn: string]: string } {
+    return this.data.primaryAccounts
+  }
+
+  get accountId(): string {
+    return this.data.primaryAccounts["urn:ietf:params:jmap:mail"]
+  }
+}
+
 export class EmailProvider {
+  static async create(config: FastmailConfig) {
+    const session = await FastmailSession.create(config.token)
+    return new this(new FastmailEmailAccount(session))
+  }
+
   static createNull({
     mailboxState,
   }: NullEmailProviderConfiguration = defaultNullConfiguration) {
-    return new this()
+    return new this(new NullEmailAccount(mailboxState))
   }
 
+  constructor(private readonly account: EmailAccount) {}
+
   getMailboxState() {
-    throw new Error("Method not implemented.")
+    return this.account.getMailboxState()
   }
 }
