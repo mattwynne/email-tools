9636186eb7aa90034e7629ed2c592abd6e97dd7b
Matt Wynne <matt@mattwynne.net>
2023-07-24 21:34:33 -0700

    Read an actual email from the inbox
    Hide credentials

--- src/core/Mailbox.ts
+++ src/core/Mailbox.ts
@@ -2,12 +2,16 @@ import { MailboxName } from "./MailboxName"
 import { Email } from "./Email"
 
 export class Mailbox {
-  static named(name: string) {
+  static named(name: string | MailboxName) {
     return new this(MailboxName.of(name), [])
   }
 
-  constructor(
+  private constructor(
     public readonly name: MailboxName,
     public readonly emails: Email[]
   ) {}
+
+  public withEmails(emails: Email[]) {
+    return new Mailbox(this.name, emails)
+  }
 }
--- src/infrastructure/EmailProvider.spec.ts
+++ src/infrastructure/EmailProvider.spec.ts
@@ -4,14 +4,16 @@ import { Mailbox } from "../core/Mailbox"
 import { EmailAddress } from "../core/EmailAddress"
 import { MailboxName } from "../core/MailboxName"
 import { EmailProvider, FastmailConfig, FastmailSession } from "./EmailProvider"
-import { assertThat, equalTo } from "hamjest"
+import { assertThat, equalTo, promiseThat } from "hamjest"
 import { EmailSubject } from "../core/EmailSubject"
+import { eventually } from "ts-eventually"
+import nodemailer from "nodemailer"
 
 describe(EmailProvider.name, () => {
   describe("null mode", () => {
     it("can be created with a stubbed MailboxState", async () => {
       const mailboxState = new MailboxState([
-        new Mailbox(MailboxName.of("Inbox/Paperwork"), [
+        Mailbox.named("Inbox/Paperwork").withEmails([
           Email.from(EmailAddress.of("someone@example.com")).about(
             EmailSubject.of("a subject")
           ),
@@ -24,9 +26,9 @@ describe(EmailProvider.name, () => {
 
     it("returns only mailboxes matching the given names", async () => {
       const mailboxState = new MailboxState([
-        new Mailbox(MailboxName.of("Inbox/Paperwork"), []),
-        new Mailbox(MailboxName.of("Inbox/Screener"), []),
-        new Mailbox(MailboxName.of("Sent"), []),
+        Mailbox.named("Inbox/Paperwork"),
+        Mailbox.named("Inbox/Screener"),
+        Mailbox.named("Sent"),
       ])
       const provider = EmailProvider.createNull({ mailboxState })
       const actual = await provider.getMailboxState([
@@ -37,21 +39,22 @@ describe(EmailProvider.name, () => {
         actual,
         equalTo(
           new MailboxState([
-            new Mailbox(MailboxName.of("Inbox/Paperwork"), []),
-            new Mailbox(MailboxName.of("Inbox/Screener"), []),
+            Mailbox.named("Inbox/Paperwork"),
+            Mailbox.named("Inbox/Screener"),
           ])
         )
       )
     })
   })
 
+  const TIMEOUT = 10000
   describe("fastmail mode", function () {
-    this.timeout(10000)
+    this.timeout(TIMEOUT)
     const fastmailConfig: FastmailConfig = {
       token: process.env.FASTMAIL_API_TOKEN || "", // TODO: make env nullable infrastructure too
     }
 
-    it("connects to a real fastmail inbox", async function () {
+    it("connects to a real, empty fastmail inbox", async function () {
       await reset(fastmailConfig.token)
       const provider = await EmailProvider.create(fastmailConfig)
       assertThat(
@@ -65,6 +68,30 @@ describe(EmailProvider.name, () => {
             Mailbox.named("Spam"),
             Mailbox.named("Trash"),
           ])
+        )
+      )
+    })
+
+    it("reads an email that's in the Inbox", async () => {
+      await reset(fastmailConfig.token)
+      await sendTestEmail(
+        Email.from(EmailAddress.of("someone@example.com")).about(
+          EmailSubject.of("a subject")
+        )
+      )
+      const provider = await EmailProvider.create(fastmailConfig)
+      await eventually(async () =>
+        assertThat(
+          await provider.getMailboxState([MailboxName.of("Inbox")]),
+          equalTo(
+            new MailboxState([
+              Mailbox.named("Inbox").withEmails([
+                Email.from(EmailAddress.of("someone@example.com")).about(
+                  EmailSubject.of("a subject")
+                ),
+              ]),
+            ])
+          )
         )
       )
     })
@@ -136,4 +163,20 @@ const reset = async (token: string) => {
       "3",
     ],
   ])
+}
+async function sendTestEmail(email: Email) {
+  const smtp = nodemailer.createTransport({
+    host: "smtp.fastmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "test@levain.codes",
+      pass: process.env.FASTMAIL_SMTP_PASSWORD,
+    },
+  })
+  await smtp.sendMail({
+    to: "test@levain.codes",
+    from: email.from.value,
+    subject: email.subject.value,
+  })
 }
