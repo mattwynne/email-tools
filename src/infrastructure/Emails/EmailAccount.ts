4fbd271c3f6ca861013ae6be3d64a40df98d58d1
Matt Wynne <matt@mattwynne.net>
2023-08-29 15:59:29 -0700

    Tidy

--- src/infrastructure/Emails/EmailAccount.ts
+++ src/infrastructure/Emails/EmailAccount.ts
@@ -4,3 +4,19 @@ import { MailboxState } from "../../core/MailboxState"
 export interface EmailAccount {
   getMailboxState: (onlyMailboxNames?: MailboxName[]) => Promise<MailboxState>
 }
+
+export class NullEmailAccount implements EmailAccount {
+  constructor(private readonly mailboxStates: MailboxState[]) {}
+
+  async getMailboxState(onlyMailboxNames?: MailboxName[]) {
+    const nextMailboxState = this.mailboxStates.shift()
+    if (!nextMailboxState)
+      throw new Error("You don't have any more configured MailboxStates")
+    if (!onlyMailboxNames) return nextMailboxState
+    return new MailboxState(
+      nextMailboxState.mailboxes.filter((mailbox) =>
+        onlyMailboxNames.some((name) => name.equals(mailbox.name))
+      )
+    )
+  }
+}
