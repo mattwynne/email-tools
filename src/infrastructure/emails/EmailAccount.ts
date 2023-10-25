091d91fafe70ac0646060000667aa4e537b59e0a
Matt Wynne <matt@mattwynne.net>
2023-10-24 18:46:41 -0700

    Remove EmailAccount abstraction

--- src/infrastructure/emails/EmailAccount.ts
+++
@@ -1,7 +0,0 @@
-import { MailboxName, MailboxState } from "../../core"
-
-export interface EmailAccount {
-  onChange: (handler: () => void) => void
-  getMailboxState: (onlyMailboxNames?: MailboxName[]) => Promise<MailboxState>
-  close: () => void
-}
--- src/infrastructure/emails/EmailProvider.ts
+++ src/infrastructure/emails/EmailProvider.ts
@@ -1,31 +1,106 @@
-import { MailboxName, MailboxState } from "../../core"
-import { EmailAccount } from "./EmailAccount"
-import { NullEmailAccount } from "./NullEmailAccount"
-import { FastmailAccount } from "./FastmailAccount"
+import {
+  Email,
+  EmailAddress,
+  EmailSubject,
+  Mailbox,
+  MailboxName,
+  MailboxState,
+} from "../../core"
 import { FastmailConfig, FastmailSession } from "./FastmailSession"
 
-export class EmailProvider {
+export class FastmailAccount {
+  static createNull(arg?: any): FastmailAccount {
+    throw new Error("Method not implemented.")
+  }
+
   static async create(config: FastmailConfig) {
     const session = await FastmailSession.create(config.token)
-    return new this(new FastmailAccount(session))
+    return new this(session)
   }
 
-  static createNull(config?: { mailboxStates?: MailboxState[] }) {
-    const { mailboxStates } = { mailboxStates: [], ...config }
-    return new this(new NullEmailAccount(mailboxStates.slice()))
+  constructor(private readonly session: FastmailSession) {}
+
+  public async getMailboxState(onlyMailboxes?: MailboxName[]) {
+    const mailboxes = await this.getMailboxes(onlyMailboxes)
+    // const inbox = mailboxes.find(
+    //   (mailbox) => mailbox.name === MailboxName.of("Inbox")
+    // )
+    // const inboxChildren = mailboxes.filter(
+    //   (mailbox: { parentId: string }) => mailbox.parentId === inbox.id
+    // )
+    return new MailboxState(mailboxes)
   }
 
-  constructor(private readonly account: EmailAccount) {}
-
-  getMailboxState(onlyMailboxNames?: MailboxName[]) {
-    return this.account.getMailboxState(onlyMailboxNames)
+  public onChange(handler: () => void) {
+    this.session.subscribe(handler)
   }
 
-  onChange(handler: () => void) {
-    this.account.onChange(handler)
+  public close() {
+    this.session.close()
   }
 
-  close() {
-    this.account.close()
+  private async getMailboxes(
+    onlyMailboxes?: MailboxName[]
+  ): Promise<Mailbox[]> {
+    const filter = onlyMailboxes
+      ? (mailbox: { name: string }) =>
+          onlyMailboxes.some((name) =>
+            name.equals(MailboxName.of(mailbox.name))
+          )
+      : () => true
+
+    return Promise.all(
+      (
+        await this.session.call("Mailbox/get", {
+          accountId: this.session.accountId,
+          ids: null,
+        })
+      ).list
+        .filter(filter)
+        .map(async (mailbox: { id: string; name: string }) =>
+          Mailbox.named(mailbox.name).withEmails(
+            await this.getEmailsIn(mailbox.id)
+          )
+        )
+    )
+  }
+
+  private async getEmailsIn(mailboxId: string): Promise<Email[]> {
+    const result = await this.session.calls([
+      [
+        "Email/query",
+        {
+          accountId: this.session.accountId,
+          filter: {
+            inMailbox: mailboxId,
+          },
+        },
+        "0",
+      ],
+      [
+        "Email/get",
+        {
+          accountId: this.session.accountId,
+          "#ids": {
+            resultOf: "0",
+            name: "Email/query",
+            path: "/ids",
+          },
+        },
+        "1",
+      ],
+    ])
+    const emails = result[1][1].list
+    if (!emails) {
+      throw new Error("No emails found for mailbox " + mailboxId)
+    }
+    return emails.map(
+      (email: { subject: string; from: { email: string }[] }) => {
+        const sender = email.from ? email.from[0].email : "unknown@example.com"
+        return Email.from(EmailAddress.of(sender)).about(
+          EmailSubject.of(email.subject)
+        )
+      }
+    )
   }
 }
