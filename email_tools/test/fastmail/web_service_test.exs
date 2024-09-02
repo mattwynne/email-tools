826019d11ee22ff3304a90e7b8066378aa698513
Matt Wynne <matt@mattwynne.net>
2024-09-01 21:28:24 -0700

    Review code, add TODOs, tidy up

--- email_tools/test/fastmail/web_service_test.exs
+++ email_tools/test/fastmail/web_service_test.exs
@@ -1,38 +1,47 @@
 defmodule Fastmail.WebServiceTest do
+  alias Fastmail.WebService
   use ExUnit.Case, async: true
 
   describe "fetching a session" do
     @tag :online
     test "it calls the fastmail servers to connect" do
       token = System.get_env("FASTMAIL_API_TOKEN")
-      web_service = Fastmail.WebService.create(token: token)
-      {:ok, session} = web_service |> Fastmail.WebService.get_session()
+      web_service = WebService.create(token: token)
+      {:ok, session} = web_service |> WebService.get_session()
       assert session.account_id == "u360641ae"
     end
 
     test "it fails with a bad token" do
-      fake = fn request ->
-        {request, Req.Response.new(status: 301, body: "Authorization header not a valid format")}
-      end
+      web_service =
+        WebService.create_null(
+          get_session: fn request ->
+            {
+              request,
+              Req.Response.new(status: 301, body: "Authorization header not a valid format")
+            }
+          end
+        )
 
-      web_service = Fastmail.WebService.create_null(get_session: fake)
-
-      {:error, error} = web_service |> Fastmail.WebService.get_session()
+      {:error, error} = web_service |> WebService.get_session()
 
       assert Exception.message(error) |> String.trim() ==
                "Authorization header not a valid format"
     end
 
     test "it fails with a bad URL" do
-      fake = fn request ->
-        {request, RuntimeError.exception("non-existing domain")}
-      end
+      web_service =
+        WebService.create_null(
+          get_session: fn request ->
+            {
+              request,
+              RuntimeError.exception("non-existing domain")
+            }
+          end
+        )
 
-      web_service = Fastmail.WebService.create_null(get_session: fake)
-      {:error, error} = web_service |> Fastmail.WebService.get_session()
+      {:error, error} = web_service |> WebService.get_session()
 
-      assert Exception.message(error) |> String.trim() ==
-               "non-existing domain"
+      assert Exception.message(error) |> String.trim() == "non-existing domain"
     end
   end
 end
