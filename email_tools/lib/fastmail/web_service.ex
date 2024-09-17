21b3ae68fad5db12234129dc6ea361a829d6baf4
Matt Wynne <matt@mattwynne.net>
2024-09-16 21:22:16 -0700

    Move responsibility for creating event stream into tested code

--- email_tools/lib/fastmail/web_service.ex
+++ email_tools/lib/fastmail/web_service.ex
@@ -1,26 +1,34 @@
 defmodule Fastmail.WebService do
-  defstruct [:get_session, :token]
+  defstruct [:get_session, :get_event_source, :token]
 
   def create_null(opts \\ []) do
     noop = fn request -> {request, Req.Response.new()} end
 
-    new(fn _token -> Req.new(adapter: opts[:get_session] || noop) end, :a_token)
+    new(%__MODULE__{
+      get_session: fn _token -> Req.new(adapter: opts[:get_session] || noop) end,
+      get_event_source: fn _token, _url -> Req.new(adapter: opts[:get_event_source] || noop) end,
+      token: :a_token
+    })
   end
 
   def create(opts) do
     token = Keyword.fetch!(opts, :token)
 
-    new(&Fastmail.Request.get_session/1, token)
+    new(%__MODULE__{
+      get_session: &Fastmail.Request.get_session/1,
+      get_event_source: &Fastmail.Request.get_event_source/2,
+      token: token
+    })
   end
 
-  defp new(get_session, token) do
-    %__MODULE__{get_session: get_session, token: token}
+  defp new(%__MODULE__{} = web_service) do
+    web_service
   end
 
   def get_session(web_service) do
     case Req.request(web_service.get_session.(web_service.token)) do
       {:ok, %{status: 200, body: body}} ->
-        {:ok, body |> Fastmail.Session.parse()}
+        {:ok, body |> Fastmail.Session.new(web_service)}
 
       {:ok, %{body: message}} ->
         {:error, RuntimeError.exception(message)}
@@ -28,5 +36,11 @@ defmodule Fastmail.WebService do
       {:error, error} ->
         {:error, error}
     end
+  end
+
+  def get_event_source(web_service, url) do
+    # TODO: handle errors
+    dbg(url)
+    Req.request(web_service.get_event_source.(web_service.token, url))
   end
 end
