13e815362aa7fda9b09ab9596214967d88769583
Matt Wynne <matt@mattwynne.net>
2024-09-19 22:45:26 -0700

    Start adding contacts

--- email_tools/lib/fastmail/request.ex
+++ email_tools/lib/fastmail/request.ex
@@ -1,29 +1,5 @@
 defmodule Fastmail.Request do
   # TODO: consider making these private methods on WebService
-  def get_session(token) do
-    Req.new(
-      method: :get,
-      url: "https://api.fastmail.com/jmap/session",
-      headers: [
-        {"accept", "application/json"}
-      ],
-      auth: {:bearer, token}
-    )
-  end
-
-  def get_event_source(token, url) do
-    Req.new(
-      method: :get,
-      url: url,
-      headers: [
-        {"accept", "text/event-stream"}
-        # "last-event-id" => state.last_event_id
-      ],
-      auth: {:bearer, token},
-      into: :self,
-      receive_timeout: :infinity
-    )
-  end
 
   def method_calls(url, token, method_calls) do
     Req.new(
