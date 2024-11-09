2b60041e5b395c53f7a4f5499be55d3e4b63ec93
Matt Wynne <matt@mattwynne.net>
2024-11-08 22:50:25 -0800

    Start working on contact cards

--- email_tools/test/fastmail/contacts/card_test.exs
+++ email_tools/test/fastmail/contacts/card_test.exs
@@ -12,6 +12,18 @@ defmodule Fastmail.Contacts.CardTest do
       assert card.uid == "f91bef7c-e405-48ac-9657-e763d5e6a279"
       assert card.rev == "20241108T063625Z"
     end
+
+    @tag :wip
+    test "parses a fastmail contact" do
+      body =
+        "BEGIN:VCARD\r\nPRODID:-//CyrusIMAP.org//Cyrus \r\n 3.11.0-alpha0-1110-gdd2947cf5-fm-20241..//EN\r\nVERSION:3.0\r\nUID:cee595dd-1819-405a-926a-2ff68119333a\r\nN:Wynne;Matt;;;\r\nFN:Matt Wynne\r\nNICKNAME:\r\nTITLE:\r\nORG:;\r\nEMAIL;TYPE=HOME;TYPE=PREF:test@test.com\r\nNOTE:\r\nREV:20241109T064005Z\r\nEND:VCARD\r\n"
+
+      card = Card.parse(body)
+      assert card.uid == "cee595dd-1819-405a-926a-2ff68119333a"
+      assert card.formatted_name == "Matt Wynne"
+      assert card.name == "Wynne;Matt"
+      assert card.email == "test@test.com"
+    end
   end
 
   describe "creating a card for a group" do
