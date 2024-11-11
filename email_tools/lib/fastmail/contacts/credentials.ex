54d181b68405d1c77d2cc6052468d2b80dc672d6
Matt Wynne <matt@mattwynne.net>
2024-11-11 10:06:42 -0800

    Add basic_auth function to Credentials

--- email_tools/lib/fastmail/contacts/credentials.ex
+++ email_tools/lib/fastmail/contacts/credentials.ex
@@ -1,10 +1,26 @@
 defmodule Fastmail.Contacts.Credentials do
+  alias Fastmail.Contacts.Credentials
   defstruct [:username, :password]
 
-  def from_environment() do
+  def new(username, password) do
     %__MODULE__{
-      username: System.fetch_env!("FASTMAIL_USERNAME"),
-      password: System.fetch_env!("FASTMAIL_DAV_PASSWORD")
+      username: username,
+      password: password
     }
+  end
+
+  def from_environment() do
+    new(
+      System.fetch_env!("FASTMAIL_USERNAME"),
+      System.fetch_env!("FASTMAIL_DAV_PASSWORD")
+    )
+  end
+
+  def basic_auth(%Credentials{username: username, password: password}) do
+    username = String.replace(username, "@", "+Default@")
+
+    digest = :base64.encode(username <> ":" <> password)
+
+    "Basic " <> digest
   end
 end
