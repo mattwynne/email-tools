ebea077111ad01e2feed2a08ad0dc06846c442be
Matt Wynne <matt@mattwynne.net>
2024-11-22 23:17:36 -0800

    Move the tests into lib

--- email_tools/mix.exs
+++ email_tools/mix.exs
@@ -9,7 +9,8 @@ defmodule EmailTools.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_paths: ["lib"]
     ]
   end
 
