101819a9111fb975d724fc263b1e567a60be2fca
Matt Wynne <matt@mattwynne.net>
2025-09-23 22:24:18 -0700

    Refactor method_calls

--- email_tools/lib/fastmail/jmap/method_calls/get_emails_by_ids.ex
+++ email_tools/lib/fastmail/jmap/method_calls/get_emails_by_ids.ex
@@ -1,5 +1,13 @@
 defmodule Fastmail.Jmap.MethodCalls.GetEmailsByIds do
+  defmodule Params do
+    defstruct [:account_id, :ids]
+  end
+
   def new(account_id, ids) do
+    new(%Params{account_id: account_id, ids: ids})
+  end
+
+  def new(%Params{} = %{account_id: account_id, ids: ids}) do
     [
       [
         "Email/get",
