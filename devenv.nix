213dd2848342dd982465a21b5db50441e0268f46
Matt Wynne <matt@mattwynne.net>
2025-11-04 22:48:35 -0800

    Split devenv for root folder and inbox_tools folder

--- devenv.nix
+++ devenv.nix
@@ -1,67 +1,18 @@
 { pkgs, lib, config, ... }:
 {
   packages = with pkgs; [
-    elixir
-    erlang
     nodejs
-    postgresql
-    flyctl
     gh
     lolcat
   ];
 
-  languages.elixir.enable = true;
-
-  services.postgres = {
-    enable = true;
-    package = pkgs.postgresql_15;
-    initialScript = ''
-      CREATE USER postgres WITH SUPERUSER PASSWORD 'postgres';
-    '';
-    initialDatabases = [
-      { name = "inbox_coach_dev"; }
-      { name = "inbox_coach_test"; }
-    ];
-    listen_addresses = "*";
-  };
-
-  scripts.setup.exec = ''
-    cd inbox_coach
-    mix deps.get
-    mix ecto.setup
-  '';
-
-  processes = {} //
-    lib.optionalAttrs (!config.devenv.isTesting) {
-      phoenix = {
-        exec = ''
-          cd inbox_coach
-          mix phx.server
-        '';
-        process-compose = {
-          depends_on = {
-            postgres = {
-              condition = "process_healthy";
-            };
-          };
-        };
-      };
-    };
+  languages.typescript.enable = true;
 
   enterShell = ''
     cat logo | lolcat -p .5
     echo
-    echo "Run 'devenv up -d' to start services"
-    echo "Run 'setup' for one-time project setup"
-  '';
-
-  enterTest = ''
-    echo "Waiting for postres to start"
-    wait_for_port 5432 30
-
-    echo "Postgres started. Getting deps and running tests..."
-    cd inbox_coach
-    mix deps.get
-    mix test.all
+    echo "TypeScript development environment"
+    echo "Run 'npm install' to install dependencies"
+    echo "Run 'npm test' to run tests"
   '';
 }
---
+++ inbox_coach/devenv.nix
@@ -0,0 +1,64 @@
+{ pkgs, lib, config, ... }:
+{
+  packages = with pkgs; [
+    elixir
+    erlang
+    nodejs
+    postgresql
+    flyctl
+    gh
+    lolcat
+  ];
+
+  languages.elixir.enable = true;
+
+  services.postgres = {
+    enable = true;
+    package = pkgs.postgresql_15;
+    initialScript = ''
+      CREATE USER postgres WITH SUPERUSER PASSWORD 'postgres';
+    '';
+    initialDatabases = [
+      { name = "inbox_coach_dev"; }
+      { name = "inbox_coach_test"; }
+    ];
+    listen_addresses = "*";
+  };
+
+  scripts.setup.exec = ''
+    mix deps.get
+    mix ecto.setup
+  '';
+
+  processes = {} //
+    lib.optionalAttrs (!config.devenv.isTesting) {
+      phoenix = {
+        exec = ''
+          mix phx.server
+        '';
+        process-compose = {
+          depends_on = {
+            postgres = {
+              condition = "process_healthy";
+            };
+          };
+        };
+      };
+    };
+
+  enterShell = ''
+    cat ../logo | lolcat -p .5
+    echo
+    echo "Run 'devenv up -d' to start services"
+    echo "Run 'setup' for one-time project setup"
+  '';
+
+  enterTest = ''
+    echo "Waiting for postres to start"
+    wait_for_port 5432 30
+
+    echo "Postgres started. Getting deps and running tests..."
+    mix deps.get
+    mix test.all
+  '';
+}
