93d77a724368b7b082db97f5c96f8a69655d2542
Matt Wynne <matt@mattwynne.net>
2026-05-11 19:54:50 -0700

    Update devenv tooling

--- devenv.nix
+++ devenv.nix
@@ -1,9 +1,26 @@
 { pkgs, lib, config, ... }:
+let
+  fastmail-cli = pkgs.stdenv.mkDerivation {
+    pname = "fastmail-cli";
+    version = "2.2.2";
+    src = pkgs.fetchurl {
+      url = "https://github.com/radiosilence/fastmail-cli/releases/download/v2.2.2/fastmail-cli-darwin-aarch64.tar.gz";
+      sha256 = "1awwh7cfd8r60ywn60gnx6ph98cq4sp7f3m57czmnbwjdpam9kgx";
+    };
+    phases = [ "installPhase" ];
+    installPhase = ''
+      mkdir -p $out/bin
+      tar -xzf $src -C $out/bin
+      chmod +x $out/bin/fastmail-cli
+    '';
+  };
+in
 {
   packages = with pkgs; [
     nodejs
     gh
     lolcat
+    fastmail-cli
   ];
 
   languages.typescript.enable = true;
